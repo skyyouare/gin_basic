18598ee4b0153c230020fd05f4b23852a6ed0aab
doujinya <doujinya@social-touch.com>
2021-03-18 17:21:19 +0800

    gorm配置优化

--- pkg/gorm/gorm.go
+++ pkg/gorm/gorm.go
@@ -2,8 +2,8 @@ package gorm
 
 import (
 	"fmt"
+	"gin_basic/pkg/logger"
 	"gin_basic/pkg/setting"
-	"log"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -11,12 +11,11 @@ import (
 	"time"
 )
 
-var db *gorm.DB
+var Conn *gorm.DB
 
 // Setup initializes the database instance
 func Setup() {
-	var err error
-	db, err = gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+	database, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		setting.MysqlSetting.UserName,
 		setting.MysqlSetting.PassWord,
 		setting.MysqlSetting.IPHost,
@@ -24,20 +23,21 @@ func Setup() {
 		setting.MysqlSetting.DbName))
 
 	if err != nil {
-		log.Fatalf("models.Setup err: %v", err)
+		logger.Fatal("gorm.Setup err: %v", err)
 	}
 
-	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+	gorm.DefaultTableNameHandler = func(database *gorm.DB, defaultTableName string) string {
 		return setting.MysqlSetting.TablePrefix + defaultTableName
 	}
 
-	db.SingularTable(true)                                                         //gorm会在创建表的时候去掉”s“的后缀
-	db.DB().SetMaxIdleConns(setting.MysqlSetting.MaxIdleConns)                     // 设置空闲链接
-	db.DB().SetMaxOpenConns(setting.MysqlSetting.MaxOpenConns)                     // 最大连接数 0为不限制
-	db.DB().SetConnMaxLifetime(setting.MysqlSetting.ConnMaxLifetime * time.Minute) // 可重用链接得最大时间长度
+	database.SingularTable(true)                                                         //gorm会在创建表的时候去掉”s“的后缀
+	database.DB().SetMaxIdleConns(setting.MysqlSetting.MaxIdleConns)                     // 设置空闲链接
+	database.DB().SetMaxOpenConns(setting.MysqlSetting.MaxOpenConns)                     // 最大连接数 0为不限制
+	database.DB().SetConnMaxLifetime(setting.MysqlSetting.ConnMaxLifetime * time.Minute) // 可重用链接得最大时间长度
+	Conn = database
 }
 
 // Close closes database connection (unnecessary)
 func Close() {
-	defer db.Close()
+	defer Conn.Close()
 }
