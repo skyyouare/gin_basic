38bb7d33caf9c69282b4c625457eb817eb4f0e3e
skyyouare <401298791@qq.com>
2023-08-10 15:03:07 +0800

    gorm2

--- controller/TestController.go
+++ controller/TestController.go
@@ -3,8 +3,8 @@ package controller
 import (
 	"fmt"
 	"gin_basic/middleware"
+	"gin_basic/model"
 	"gin_basic/pkg/gorm"
-	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,20 +19,11 @@ func TestRegister(router *gin.RouterGroup) {
 	router.GET("/test/test", controller.test)
 }
 
-type Auth struct {
-	ID        uint      `gorm:"column:id;primary_key"` //primary_key:设置主键
-	Username  string    `gorm:"column:username;type:varchar(100)"`
-	Password  string    `gorm:"column:password;type:varchar(100)"`
-	CreatedAt time.Time `gorm:"column:created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at"`
-}
-
 // 测试
 func (t *TestController) test(c *gin.Context) {
-	// middleware.ResponseError(c, middleware.ErrorCode, errors.New("测试"))
 	// blog_auth
-	var auth Auth
-	gorm.Conn.First(&auth)
-	fmt.Println(auth, auth.ID, auth.Username, auth.Password)
+	var authlist []model.Auth
+	gorm.Conn.Find(&authlist)
+	fmt.Println(authlist)
 	middleware.ResponseSuccess(c, "test")
 }
